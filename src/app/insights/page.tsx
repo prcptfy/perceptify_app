e931a90ced148b5074d69525c59ba937fbf297a9
Aidan Jiang <aidanjiang9@gmail.com>
2023-05-27 21:23:44 -0600

    add initial page

---
+++ src/app/insights/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Sidebar from '@/components/sidebar/Sidebar';
+
+const Insights = () => {
+  return (
+    <div className="flex flex-row">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Insights;
