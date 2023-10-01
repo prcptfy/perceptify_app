8fd5b470eea8f10fd96d8283da438f4e73fd267f
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-09-30 21:33:01 -0700

    fghfghfghghffgh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,57 +15,36 @@ type SessionType = {
   access_token: string;
 };
 
-export default async function RootLayout({
-  children,
-}: {
+interface RootLayoutProps {
   children: React.ReactNode;
-}) {
+}
+
+export default async function RootLayout({ children }: RootLayoutProps) {
   const supabase = createServerClient();
 
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  if (session) {
-    const typeSession = session as SessionType;
-    return (
-      <html lang="en">
-        <body>
-          <SupabaseProvider session={session}>
-            <SupabaseListener serverAccessToken={typeSession?.access_token} />
-            <main className="flex ">
-              <div className="w-74 mr-4 border-r border-gray-200 bg-white ">
-                <Sidebar />
-              </div>
-              <div className="ml-64 w-auto flex-grow overflow-auto bg-white p-4">
-                {children}
-              </div>
-            </main>
-          </SupabaseProvider>
-        </body>
-      </html>
-    );
-} else {
-  
   return (
     <html lang="en">
       <body>
         <SupabaseProvider session={session}>
-          <SupabaseListener serverAccessToken={session?.access_token} />
-          <main className="flex min-h-screen main">
-            <div className='sidebar'>
-              {/* <Sidebar/> */}
+          {session && (
+            <SupabaseListener
+              serverAccessToken={(session as SessionType)?.access_token}
+            />
+          )}
+          <main className="flex ">
+            <div className="w-74 mr-4 border-r border-gray-200 bg-white ">
+              {session && <Sidebar />}
             </div>
-            <div className="">
-              <div className=''>
-                {children}
-              </div>
+            <div className="ml-64 w-auto flex-grow overflow-auto bg-white p-4">
+              {children}
             </div>
           </main>
         </SupabaseProvider>
       </body>
     </html>
   );
-}
-
 }
