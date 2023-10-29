be899a6564523f458157e800896589cc22d73538
kevin <zhang.kevin.hua@gmail.com>
2023-10-29 18:10:53 -0500

    merge

--- src/app/register/magic-link-redirect/page.tsx
+++ src/app/register/magic-link-redirect/page.tsx
@@ -1,6 +1,12 @@
 'use client'
 
-const magicLinkRedirect = async () =>
+import { useSupabase } from "@/components/supabase-provider";
+
+const magicLinkRedirect = async () => {
+    const { session, supabase } = useSupabase();
+    console.log(session);
+    if (!session) return null;
     location.href='/register/create-profile';
+}
 
 export default magicLinkRedirect;
